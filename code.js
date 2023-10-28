2d605fb514530e1a87eb280babbebd8497634470
oron01 <136211946+oron01@users.noreply.github.com>
2023-10-28 21:46:47 +0300

    add whole pseudocode in a broad manner

---
+++ code.js
@@ -0,0 +1,15 @@
+//Create a rock paper scissors game in which a player plays vs computer
+
+//initialize required variables (round, score)
+
+//Determine Round
+
+//Get players rps choices
+
+//Determine winner
+
+//Announce winner
+
+//Recorder Winner + Round
+
+//Determine score
